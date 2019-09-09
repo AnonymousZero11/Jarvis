4505df087846c95e480fb824cf67c73471f30605
AnonymousZero <AnonymousZero@522f9054-de74-408e-b032-a9061835c3c4>
2019-09-09 05:49:41 +0000

    Project Jarvis

---
+++ trunk/Jarvis/Jarvis/Form2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace Jarvis
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void speak(string msg)
+        {
+            SpeechSynthesizer _synthesizer = new SpeechSynthesizer();
+            _synthesizer.Speak(msg);
+            _synthesizer.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            speak(textBox1.Text);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            speak("Closing this window.");
+            this.Dispose();
+        }
+    }
+}
